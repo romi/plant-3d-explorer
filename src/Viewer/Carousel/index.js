649b347b8c5c9136891d52939b6b441e1dfbfec2
Petit Soldat <forestier.mk@gmail.com>
2019-03-08 15:49:07 +0100

    add carousel

---
+++ src/Viewer/Carousel/index.js
@@ -0,0 +1,168 @@
+import React, { useMemo, useState, useRef, useEffect } from 'react'
+import { useWindowSize } from 'react-use'
+
+import { styled } from 'rd/nano'
+import { scaleCanvas } from 'rd/tools/canvas'
+
+import { useScan } from 'flow/scans/accessors'
+import { basename } from 'common/routing'
+
+import { forgeCameraPoints } from '../World/index'
+import useImgLoader from './lodaer'
+
+const moduleHeight = 100
+
+const Container = styled.div({
+  width: '100%',
+  height: moduleHeight,
+  background: '#1f2426'
+})
+
+const Canvas = styled.canvas({
+  width: '100%',
+  height: '100%',
+  cursor: 'pointer'
+})
+
+const getSize = (elem) => elem.getBoundingClientRect()
+
+export default function Carousel () {
+  const windowSider = useWindowSize()
+  const [scan] = useScan()
+  const [urlList, setUrlList] = useState([])
+  const [context, setContext] = useState(null)
+  const [hovered, setHovered] = useState(null)
+  const [picturesLayout, setPicturesLayout] = useState([])
+  const canvasRef = useRef(null)
+  const cameraPoses = useMemo(
+    () => forgeCameraPoints(scan && scan.camera.poses),
+    [scan]
+  )
+  const [imgs] = useImgLoader(urlList)
+
+  useEffect(
+    () => {
+      setUrlList(
+        cameraPoses.map((d) => {
+          return `${basename}data/${d.photoUri}`
+        })
+      )
+    },
+    [cameraPoses]
+  )
+
+  useEffect(
+    () => {
+      const { width, height } = getSize(canvasRef.current)
+      const context = canvasRef.current.getContext('2d')
+      scaleCanvas(canvasRef.current, context, width, height)
+      context.width = width
+      context.height = height
+      setContext(context)
+    },
+    [windowSider, canvasRef.current]
+  )
+
+  useEffect(
+    () => {
+      if (context) {
+        const { width, height } = getSize(canvasRef.current)
+        const large = moduleHeight * (6000 / 4000)
+        const sizes = {
+          width,
+          large,
+          normal: (width / cameraPoses.length),
+          block: (
+            (
+              width - (hovered ? large : 0)
+            ) /
+            (
+              hovered
+                ? cameraPoses.length - 1
+                : cameraPoses.length
+            )
+          )
+        }
+
+        let last = { x: 0, width: 0, normalX: 0, normalWidth: 0 }
+        setPicturesLayout(
+          cameraPoses.map((d, i) => {
+            const isHovered = d === hovered
+            const x = last.x + last.width
+            const normalX = last.normalX + last.normalWidth
+
+            const obj = {
+              item: d,
+              x,
+              normalX,
+              width: isHovered ? sizes.large : sizes.block,
+              normalWidth: sizes.normal,
+              height,
+              hovered: isHovered
+            }
+
+            last = obj
+
+            return obj
+          })
+        )
+      }
+    },
+    [windowSider, context, cameraPoses, hovered]
+  )
+
+  useEffect(
+    () => {
+      if (context) {
+        const { width, height } = getSize(canvasRef.current)
+        const large = moduleHeight * (6000 / 4000)
+        context.clearRect(0, 0, width, height)
+
+        picturesLayout.forEach((d, i) => {
+          if (imgs[`${basename}data/${d.item.photoUri}`]) {
+            const imgWidth = imgs[`${basename}data/${d.item.photoUri}`].width
+            const imgHeight = imgs[`${basename}data/${d.item.photoUri}`].height
+            const ratio = imgWidth / large
+            const sx = (imgWidth / 2) - (d.width * ratio * 0.5)
+            const sy = 0
+
+            context.globalAlpha = d.hovered ? 1 : 0.5
+            context.drawImage(
+              imgs[`${basename}data/${d.item.photoUri}`],
+              sx,
+              sy,
+              d.width * ratio,
+              imgHeight,
+
+              d.x,
+              0,
+              d.width,
+              height
+            )
+          } else {
+            context.fillStyle = d.hovered ? 'white' : 'grey'
+            context.fillRect(
+              d.x,
+              0,
+              d.width,
+              height
+            )
+            context.fillStyle = 'black'
+          }
+        })
+      }
+    },
+    [context, picturesLayout, imgs]
+  )
+
+  return <Container>
+    <Canvas
+      $ref={canvasRef}
+      onMouseMove={(e) => {
+        const pictureHovered = picturesLayout.find((d) => d.normalX <= e.clientX && (d.normalX + d.normalWidth) >= e.clientX)
+        setHovered(pictureHovered ? pictureHovered.item : null)
+      }}
+      onMouseOut={(e) => setHovered(null)}
+    />
+  </Container>
+}
--- src/Viewer/index.js
+++ src/Viewer/index.js
@@ -2,17 +2,49 @@ import React from 'react'
 
 import { styled } from 'rd/nano'
 
-import World from './world'
-import Controls from './controls'
+import World from './World'
+import Controls from './Controls'
+import Carousel from './Carousel'
 
 const Container = styled.div({
   width: '100vw',
   height: '100vh'
 })
 
+const ModuleContainer = styled.div({
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column'
+})
+
+const TopContainer = styled.div({
+  display: 'flex',
+  width: '100%',
+  flexGrow: 1
+})
+
+const BottomContainer = styled.div({
+  display: 'flex',
+  width: '100%'
+})
+
+const AnglesTestBlock = styled.div({
+  width: 1,
+  height: '100%'
+})
+
 export default function Viewer (props) {
   return <Container>
-    <World />
     <Controls />
+    <ModuleContainer>
+      <TopContainer>
+        <World />
+        <AnglesTestBlock />
+      </TopContainer>
+      <BottomContainer>
+        <Carousel />
+      </BottomContainer>
+    </ModuleContainer>
   </Container>
 }
