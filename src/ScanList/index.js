9528cb99f063f4bd19715eabc8b56c37d8a6dd28
hanappe <hanappe@csl.sony.fr>
2020-03-10 10:29:43 +0100

    license header

--- src/ScanList/index.js
+++ src/ScanList/index.js
@@ -1,3 +1,31 @@
+/*
+
+Romi 3D PlantViewer: An browser application for 3D scanned
+plants.
+
+Copyright (C) 2019-2020 Sony Computer Science Laboratories
+              & Centre national de la recherche scientifique (CNRS)
+
+Authors:
+Nicolas Forestier, Ludovic Riffault, Léo Gourven, Benoit Lucet (DataVeyes)
+Timothée Wintz, Peter Hanappe (Sony CSL)
+Fabrice Besnard (CNRS)
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the GNU Affero General Public
+License as published by the Free Software Foundation, either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public
+License along with this program.  If not, see
+<https://www.gnu.org/licenses/>.
+
+*/
 import React, { useState } from 'react'
 import { omit } from 'lodash'
 import { FormattedMessage } from 'react-intl'
