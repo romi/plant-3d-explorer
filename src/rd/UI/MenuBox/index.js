3fcae99c96a39ae2baed7472556f21dd2daaba00
Raphaël Colin <raphael.colin68@gmail.com>
2020-06-05 11:55:33 +0200

    Changed the MenuBox so its content center when popping up

--- src/rd/UI/MenuBox/index.js
+++ src/rd/UI/MenuBox/index.js
@@ -1,8 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, Children, cloneElement, useState } from 'react'
 
+import useBB from 'rd/tools/hooks/bb'
 import styled from '@emotion/styled'
 
+const InvisibleContent = styled.div({
+  pointerEvents: 'none',
+  display: 'block',
+  minWidth: '25px',
+  height: 'auto',
+  position: 'static',
+  visibility: 'hidden'
+})
+
 export default function (props) {
+  const contentRef = useRef()
+  const [ref, BB] = useBB(false)
+  const [contentBb, setContentBb] = useState()
+
   const childrenWithoutContent = props.children
     .filter((d) => d.type !== MenuBoxContent)
   const childrenWithContent = props.children
@@ -10,13 +24,30 @@ export default function (props) {
 
   useEffect(props.callOnChange, props.watchChange)
 
+  useEffect(
+    () => {
+      if (contentRef.current) {
+        setContentBb(contentRef.current.children[0].getBoundingClientRect())
+      }
+    },
+    [contentRef.current]
+  )
+
   return <div
+    ref={ref}
   >
     {childrenWithoutContent}
     {
       props.activate
-        ? childrenWithContent
+        ? Children.map(childrenWithContent, (child) => {
+          return cloneElement(child, { parentBb: BB, contentBb: contentBb })
+        })
         : null
+    }
+    {
+      !contentBb && <InvisibleContent ref={contentRef}>
+        {childrenWithContent}
+      </InvisibleContent>
     }
   </div>
 }
@@ -31,18 +62,25 @@ const ContentContainer = styled.div({
   background: 'white',
   marginTop: 10,
   zIndex: 1000
-}/* , (props) => {
+}, (props) => {
   return {
-    marginLeft: props.contentWidth
-      ? props.contentWidth / 2
+    top: (props.contentBb && props.top)
+      ? -props.contentBb.height - 20
+      : 'normal',
+    marginLeft: props.contentBb
+      ? (
+        -(props.contentBb.width || 0) / 2
+      ) + (props.parentBb.width * 0.5)
       : 0
   }
-} */)
+})
 
 export function MenuBoxContent (props) {
   return <ContentContainer
     top={props.top}
     className={props.className}
+    parentBb={props.parentBb}
+    contentBb={props.contentBb}
     style={props.style}
   >
     <div>
