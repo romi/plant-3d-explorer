bed7c26523d9b55090ca67b8df019157344345a8
Raphaël Colin <raphael.colin68@gmail.com>
2020-06-12 16:29:45 +0200

    Program now logs which organ has been clicked in the 3d view

--- src/Viewer/World/behaviors/viewport3d.js
+++ src/Viewer/World/behaviors/viewport3d.js
@@ -1,12 +1,29 @@
 import { useState } from 'react'
 
 export default function useViewport3d (width, height) {
-  const reset = () => {}
-  const [result] = useState(null)
+  const [result, setResult] = useState({})
+  /* This is used to prevent clicking if the user has
+    just been clicking to move the camera. If it's been
+    tool long since the user pressed mouseDown, mouseUp won't
+    notify of a clic */
+  const [dateDown, setDateDown] = useState(Date.now())
+
+  const reset = () => {
+  }
 
   const eventFns = {
-    onMouseMove: (e) => {
+    onMouseUp: (e) => {
+      if (Date.now() - 1000 < dateDown) {
+        setResult({
+          ...result,
+          clicked: true
+        })
+      }
+    },
+    onMouseDown: () => {
+      setDateDown(Date.now())
     }
+
   }
 
   return [
