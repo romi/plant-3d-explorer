3a8b910e6388c173048899c0242dec0ed43db53c
Raphaël Colin <raphael.colin68@gmail.com>
2020-06-16 11:00:44 +0200

    Changed bubble color to color of clicked organ

--- src/Viewer/Interactors/overlay.js
+++ src/Viewer/Interactors/overlay.js
@@ -1,25 +1,32 @@
 import React, { useEffect, useState } from 'react'
 
 import MenuBox, { MenuBoxContent } from 'rd/UI/MenuBox'
-import { useOrganInfo } from 'flow/interactions/accessors'
+import { useOrganInfo, useColor } from 'flow/interactions/accessors'
 
 import { H2 } from 'common/styles/UI/Text/titles'
 import useMouse from 'rd/tools/hooks/mouse'
 
 function Bubble (props) {
+  const [colors] = useColor()
+  const bubbleColor = colors.organs.length >= props.organInfo &&
+    colors.organs[props.organInfo - 1]
+    ? colors.organs[props.organInfo - 1]
+    : colors.globalOrganColors[props.organInfo % 2 ? 0 : 1]
   return <div
     style={{
       position: 'fixed',
       top: props.initialPos.y,
       left: props.initialPos.x,
-      opacity: 0.7,
       zIndex: 2000
     }}>
     <MenuBox
       activate
       {...props.menuBox}
     >
-      <MenuBoxContent
+      <MenuBoxContent style={{
+        backgroundColor: bubbleColor + '30' // Added for opacity on background
+        // only
+      }}
       >
         <div
         >
