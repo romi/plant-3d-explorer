9104c57081dc573f4d3cf33286f9a90d9271a5b7
Jonathan LEGRAND <jonathan.legrand@ens-lyon.fr>
2025-02-09 17:55:35 +0100

    Add comments in `gatsby-node.js` for better clarity

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,14 @@
-const path = require('path')
+const path = require('path') // Importing 'path' module to handle and manipulate file and directory paths
 
+// Function to modify Webpack configuration during Gatsby's build process
 exports.onCreateWebpackConfig = args => {
+  // Calling Gatsby's API to set custom Webpack configuration
   args.actions.setWebpackConfig({
     resolve: {
       modules: [
-        path.resolve(__dirname, '../src'),
-        'node_modules']
+        path.resolve(__dirname, '../src'), // Resolve imports starting from the '../src' directory (absolute path)
+        'node_modules' // Default resolution for third-party modules in 'node_modules'
+      ]
     }
   })
 }
