b2916f0c42bcf99e26b4985dee100bea225bf1c0
Jonathan LEGRAND <jonathan.legrand@ens-lyon.fr>
2025-02-09 17:54:24 +0100

    Refactor comments in `doczrc.js` for better clarity

--- doczrc.js
+++ doczrc.js
@@ -1,12 +1,12 @@
 export default {
-  ignore: ['README.md'],
-  title: 'ROMI Plant 3D Explorer',
-  base: '/plant-3d-explorer/',
-  hashRouter: true,
+  ignore: ['README.md'], // Files to ignore (e.g., README.md will not be processed)
+  title: 'ROMI Plant 3D Explorer', // Application title displayed in the UI
+  base: '/plant-3d-explorer/', // Base URL for the application deployment (used for routing and assets)
+  hashRouter: true, // Use hash-based routing (applicable for SPAs to ensure compatibility with static hosting)
   menu: [
-    'Home',
-    'Project architecture',
-    'Redux store',
-    'Components'
+    'Home', // Main navigation option for the homepage
+    'Project architecture', // Menu item redirecting to the project architecture documentation
+    'Redux store', // Section to explore Redux store structure
+    'Components' // Section dedicated to the component details and documentation
   ]
 }
