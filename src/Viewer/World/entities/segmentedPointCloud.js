46fffbc35d2d119aabfb43dfc12fe982cf2f7363
Jonathan LEGRAND <jlegrand@gmail.com>
2026-07-06 17:43:40 +0200

    Fix typo in segmentedPointCloud: replace `setdAttribute` with `setAttribute`

--- src/Viewer/World/entities/segmentedPointCloud.js
+++ src/Viewer/World/entities/segmentedPointCloud.js
@@ -187,7 +187,7 @@ export default class SegmentedPointCloud extends PointCloud {
       color.toArray(this.colorsArray, i * 3)
     })
     this.geometry.removeAttribute('customColor')
-    this.geometry.setdAttribute('customColor',
+    this.geometry.setAttribute('customColor',
       new THREE.BufferAttribute(this.colorsArray, 3))
   }
 }
